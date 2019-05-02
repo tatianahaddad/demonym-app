136b27020ee87b2a565922f2fdc8b671970dad93
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-05-01 21:47:14 -0700

    updated countries for api calls

---
+++ promises.js
@@ -0,0 +1,25 @@
+function doTask(name) {
+  const p = new Promise((resolve, reject) => {
+    console.log(`${name} has started`);
+    const duration =   Math.floor(Math.random() * 5000);
+    setTimeout(() => {
+      resolve(`${name} has ended after ${duration} milliseconds`);
+    }, duration);
+  });
+  return p;
+}
+
+Promise
+  .all([
+    doTask('A'),
+    doTask('B'),
+    doTask('C')
+  ])
+  .then(results => {
+    //first console.log the results
+    results.forEach(result => console.log(result));
+    return doTask('D');
+  })
+  .then(result => {
+    console.log(result);
+  });
